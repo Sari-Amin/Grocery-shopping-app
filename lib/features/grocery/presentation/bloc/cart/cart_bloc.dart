1a20aacb38aee5ec2ff20ee081c397d745b2b076
Sari Amin <sariamin435@gmail.com>
2024-08-27 18:23:34 +0300

    finished cart bloc

--- lib/features/grocery/presentation/bloc/cart/cart_bloc.dart
+++ lib/features/grocery/presentation/bloc/cart/cart_bloc.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../Domain/entity/grocery_entity.dart';
@@ -7,31 +5,28 @@ import 'cart_event.dart';
 import 'cart_state.dart';
 
 class CartBloc extends Bloc<CartEvent, CartState> {
-  List<GroceryEntity> _cartItems = [];
+  final List<GroceryEntity> _cartItems = [];
 
-  CartBloc() : super(CartInitial());
-
-  on<AddItemEvent>((event, emit) {
+  CartBloc() : super(CartInitial()) {
+    on<AddItemEvent>((event, emit) {
       _cartItems.add(event.item);
-      emit(CartUpdatedState(cartItems: _cartItems));
+      emit(CartUpdatedState(_cartItems));
       return;
-  });
-  on<RemoveItemEvent>((event, emit) {
-    _cartItems.remove(event.item);
-    emit(CartUpdatedState(cartItems: _cartItems));
+    });
+    on<RemoveItemEvent>((event, emit) {
+      _cartItems.remove(event.item);
+      emit(CartUpdatedState(_cartItems));
       return;
+    });
 
-  });
-
-  on<ClearCartEvent>((event, emit) {
+    on<ClearCartEvent>((event, emit) {
       _cartItems.clear();
-      return emit(CartUpdatedState(cartItems: _cartItems));
-      return;
+      return emit(CartUpdatedState(_cartItems));
     });
 
-  on<GetAllCartEvent>((event, emit) {
-      emit(CartUpdatedState(cartItems: _cartItems));
+    on<GetAllCartEvent>((event, emit) {
+      emit(CartUpdatedState(_cartItems));
       return;
-
     });
+  }
 }
